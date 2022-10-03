e0f1930e615b543e4bf010a60a2d6123c06fd62d
Borja Montseny <borja.montseny@gmail.com>
2022-10-03 14:01:55 +0200

    .

--- src/Ex08.java
+++ src/Ex08.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class Ex08 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*
+		 * Escriu un programa que demani els anys treballats d’un treballador que
+		 * cobra 40.000 euros anuals i digui el sou que li correspon segons els següents
+		 * criteris:
+		 */
+
+		double souBase = 40000;
+		
+		System.out.print("Anys treballats: ");
+		Scanner sc = new Scanner(System.in);
+		int anysTreballats = sc.nextInt();
+		sc.close();
+		
+		if (anysTreballats >= 10) {
+			souBase = souBase * 1.10;
+			System.out.println("El sou que et pertoca es de " + souBase + "€.");
+			
+		} else if (anysTreballats < 10 && anysTreballats >= 5) {
+			souBase = souBase * 1.07;
+			System.out.println("El sou que et pertoca es de " + souBase + "€.");
+			
+		} else if (anysTreballats < 5 && anysTreballats >= 3) {
+			
+		}
+		
+		
+		
 	}
 
 }
